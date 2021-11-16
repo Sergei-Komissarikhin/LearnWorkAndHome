f7f7565081c20705db33b1cafee76c29bf8aa64c
Sergei Komissarikhin <Komissarihin.s@yandex.ru>
2021-11-16 22:37:25 +0300

    From home to work.

--- src/main/java/com/komissarikhin/springmvchibernate/controllers/UsersController.java
+++ src/main/java/com/komissarikhin/springmvchibernate/controllers/UsersController.java
@@ -1,12 +1,11 @@
 package com.komissarikhin.springmvchibernate.controllers;
 
 import com.komissarikhin.springmvchibernate.dao.PersonDAO;
+import com.komissarikhin.springmvchibernate.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -29,7 +28,39 @@ public class UsersController {
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
-        model.addAttribute("person",personDAO.show(id));
+        model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("{id}")
+    public String update(@ModelAttribute("person") Person person,
+                         @PathVariable("id") int id) {
+        personDAO.update(person, id);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping({"{id}"})
+    public String delete(@PathVariable("id") int id) {
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
+
+
 }
--- src/main/java/com/komissarikhin/springmvchibernate/dao/PersonDAO.java
+++ src/main/java/com/komissarikhin/springmvchibernate/dao/PersonDAO.java
@@ -27,4 +27,18 @@ public class PersonDAO {
                 .filter(person -> person.getId() == id)
                 .findAny().orElse(null);
     }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(Person updPerson, int id) {
+        Person personToBeupd = show(id);
+        personToBeupd.setName(updPerson.getName());
+    }
+    public void delete(int id){
+        people.removeIf(person -> person.getId() == id);
+        }
+
 }
